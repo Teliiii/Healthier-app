ade7ce42666aa6f8023aaf00d36e64b259b51fe4
Teliiii <ltropilet@gmail.com>
2025-03-23 14:55:27 -0700

    hi

--- calories_calculator.py
+++ calories_calculator.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
 llm = ChatOpenAI(
-    api_key = "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"
+    api_key = ""
 )
 
 print("Enter your current height in inches")
