4fec1991bc2070b9737e03d0745d01771376a6af
Clojie <eugeneyang08@gmail.com>
2025-03-23 10:40:25 -0700

    verry secwet

--- llm.py
+++ llm.py
@@ -1,6 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from langchain_openai import ChatOpenAI
 llm = ChatOpenAI(
-    api_key = "sk-ycFpnouWkNuSL_wlXEAV0nOotkyq93vd3TU6TUCD_fT3BlbkFJfMWzsbh13DnU3MDVjws7psbq_Gug0Ovx5e5TtUDCAA"
+    
 )
 
 response = llm.invoke("whats your mothers name")
